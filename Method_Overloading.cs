0640a1afc69f16edf77f0b4fda2d2cad9f85a121
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-08 14:19:29 +0530

    Add files via upload

---
+++ Method_Overloading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace PolymorphismWithCsharp
+{//Static Polymorphism or Compilr timr polymorphism is achieved by
+//Method Overloading
+//This is an example of Method Overloading
+    class Method_Overloading
+    {
+        void sum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("{0}+{1}={2}", a,b,result);
+        }
+        void sum(float a, float b)
+        {
+            float result = a + b;
+            Console.WriteLine("{0}+{1}={2}", a, b, result);
+        }
+        void sum(int a, int b, int c)
+        {
+            int result = a + b + c;
+            Console.WriteLine("{0}+{1}+{2}={3}", a, b,c, result);
+        }
+        void sum(double a, double b)
+        {
+            double result = a + b;
+            Console.WriteLine("{0} + {1} = {2}", a,b,result);
+        }
+        static void Main(string[] args)
+        {
+            Method_Overloading obj = new Method_Overloading();
+            obj.sum(45, 5);
+            obj.sum(41.325, 8.95);
+            obj.sum(78, 94, 102);
+            obj.sum(74d, 8d);
+            Console.ReadLine();
+        }
+    }
+}
